5877460a3d2b9f619d4a945b51b1f71c270489f9
Ivan Sanchez <edgarse1945@gmail.com>
2021-07-04 15:38:30 -0500

    Actualizacion de la app

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const path = require("path")
 const app = express()
 
 
+// puerto
+const PORT = process.env.PORT || 3000;
+
+// recursos publicos 
 app.use('/static', express.static('public'))
 
-
+// rutas
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "public/views/home.html"))
 })
-app.get("/register", (req, res)=>{
+
+app.get("/home", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/views/home.html"))
+})
+
+app.get("/singup", (req, res)=>{
     res.sendFile(path.join(__dirname, "public/views/registro.html"))
 })
 
-app.get("/getin", (req, res)=>{
+app.get("/login", (req, res)=>{
     res.sendFile(path.join(__dirname, "public/views/getIn.html"))
 })
 
-
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server running on localhost:3030")
+// inicializacion del servidor
+app.listen(PORT, ()=>{
+    console.log(`server running on localhost:${PORT}`);
 })
